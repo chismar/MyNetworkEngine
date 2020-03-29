719776fef183bc0ab0346d83efe313bb070f8a90
Vitaly Bulanenkov <v.bulanenkov@enplexgames.com>
2020-03-29 19:26:31 +0300

    кувырки и FXы

---
+++ TalesWatcher/Assets/UnityClient/FXScripts/FXRunner.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ParamType
+{
+    Float,
+    FloatViaCurve,
+    Trigger,
+    Bool,
+    Int
+}
+
+public class FXRunner : MonoBehaviour
+{
+    public ParamType ParamType;
+    public string ParamName;
+    public AnimationCurve FloatCurve;
+    public bool BoolValue;
+    public float FloatValue;
+    public int IntValue;
+    public float Period = 1f;
+    // Start is called before the first frame update
+
+    IEnumerator PlayCoroutine()
+    {
+        while (true)
+        {
+
+            yield return new WaitForSeconds(Period);
+            switch (ParamType)
+            {
+                case ParamType.Float:
+                    GetComponent<Animator>().SetFloat(ParamName, FloatValue);
+                    break;
+                case ParamType.FloatViaCurve:
+                    {
+                        float currentTime = 0;
+                        while(currentTime < Period)
+                        {
+                            currentTime += Time.deltaTime;
+                            GetComponent<Animator>().SetFloat(ParamName, FloatCurve.Evaluate(currentTime));
+                            yield return null;
+                        }
+                    }
+                    break;
+                case ParamType.Trigger:
+                    GetComponent<Animator>().SetTrigger(ParamName);
+                    break;
+                case ParamType.Bool:
+                    GetComponent<Animator>().SetBool(ParamName, BoolValue);
+                    break;
+                case ParamType.Int:
+                    GetComponent<Animator>().SetInteger(ParamName, IntValue);
+                    break;
+            }
+
+        }
+    }
+    Coroutine c;
+    private void OnEnable()
+    {
+        c = StartCoroutine(PlayCoroutine());        
+    }
+
+    private void OnDisable()
+    {
+        StopCoroutine(c);
+    }
+}
---
+++ TalesWatcher/Assets/UnityClient/FXScripts/TimelineState.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Animations;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+public class TimelineState : StateMachineBehaviour
+{
+    public TimelineAsset TimelineAsset;
+
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex, AnimatorControllerPlayable controller)
+    {
+        base.OnStateEnter(animator, stateInfo, layerIndex);
+        var directors = animator.GetComponentsInChildren<PlayableDirector>();
+        var director = directors.FirstOrDefault(x => x.playableAsset == null);
+        if (director != null)
+            StartPlay(animator, stateInfo, director);
+    }
+
+    private void StartPlay(Animator animator, AnimatorStateInfo stateInfo, PlayableDirector director)
+    {
+        var length = stateInfo.length;
+        var speedMultiplier = stateInfo.speedMultiplier;
+        var duration = TimelineAsset.duration;
+        var speed = duration / (length / speedMultiplier);
+        director.playableAsset = TimelineAsset;
+        foreach (var outputTrack in TimelineAsset.outputs)
+        {
+            if (outputTrack.sourceObject is ControlTrack ct)
+            {
+                foreach (TimelineClip timelineClip in ct.GetClips())
+                {
+                    ControlPlayableAsset playableAsset = (ControlPlayableAsset)timelineClip.asset;
+
+                    // set the reference of the nested timeline to the parent playable asset
+                    director.SetReferenceValue(playableAsset.sourceGameObject.exposedName, animator.gameObject);
+                    //// rebind the playableGraph of the parent timeline director
+                    //director.RebindPlayableGraphOutputs();
+                }
+            }
+            director.SetGenericBinding(outputTrack.sourceObject, animator.gameObject);
+        }
+        director.Play();
+        director.playableGraph.GetRootPlayable(0).SetSpeed(speed);
+    }
+
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex, AnimatorControllerPlayable controller)
+    {
+        base.OnStateExit(animator, stateInfo, layerIndex, controller);
+        var directors = animator.GetComponentsInChildren<PlayableDirector>();
+        var director = directors.Where(x=>x.playableAsset==TimelineAsset).OrderByDescending(x=>x.time).First();
+        if (director != null)
+            StopPlay(director);
+
+    }
+
+    private static void StopPlay(PlayableDirector director)
+    {
+        director.Stop();
+        director.playableAsset = null;
+    }
+}
